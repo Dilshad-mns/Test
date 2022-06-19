d702ba932ace5d0728581f40d4c17f04b0341ba2
P9169174 <P9169174@WVDFODW10VM-6>
2022-06-19 04:21:24 +0000

    testing the endpoint

--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.Test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import com.example.Test.controller.StudentsController;
+
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 class TestApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+//	@Autowired
+//	private StudentsController studentsController;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(restTemplate.getForObject("http://localhost:"+port+"/hello", String.class)).contains("Hello There");
 	}
 
 }
